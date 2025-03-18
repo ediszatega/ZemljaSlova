2a97f313a22ea44ba613da3f52b1c7f17faaa0f4
Edis Zatega <ediszatega@hotmail.com>
2025-03-18 23:31:44 +0100

    wip: add jwt

--- ZemljaSlova.API/Controllers/AuthorController.cs
+++ ZemljaSlova.API/Controllers/AuthorController.cs
@@ -3,9 +3,11 @@ using ZemljaSlova.Model;
 using ZemljaSlova.Model.Requests;
 using ZemljaSlova.Model.SearchObjects;
 using ZemljaSlova.Services;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ZemljaSlova.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class AuthorController : BaseCRUDController<Model.Author, AuthorSearchObject, AuthorUpsertRequest, AuthorUpsertRequest>
--- ZemljaSlova.Services/UserService.cs
+++ ZemljaSlova.Services/UserService.cs
@@ -7,13 +7,57 @@ using MapsterMapper;
 using ZemljaSlova.Model.Requests;
 using ZemljaSlova.Model.SearchObjects;
 using ZemljaSlova.Services.Database;
-
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
 namespace ZemljaSlova.Services
 {
     public class UserService : BaseCRUDService<Model.User, UserSearchObject, Database.User, UserUpsertRequest, UserUpsertRequest>, IUserService
     {
-        public UserService(_200036Context context, IMapper mapper) : base(context, mapper)
+        private readonly IConfiguration _configuration;
+        public UserService(_200036Context context, IMapper mapper, IConfiguration configuration) : base(context, mapper)
         {
+            _configuration = configuration;
+        }
+
+        public async Task<AuthenticationResponse> AuthenticateUser(string email, string password)
+        {
+            var user = context.Users.FirstOrDefault(u => u.Email == email);
+
+            if (user == null)
+            {
+                return new AuthenticationResponse { Result = Util.AuthenticationResult.UserNotFound };
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(password, user.Password))
+            {
+                return new AuthenticationResponse { Result = Util.AuthenticationResult.InvalidPassword };
+            }
+
+            var token = CreateToken(user);
+
+            return new AuthenticationResponse { Result = Util.AuthenticationResult.Success, UserId = user.UserId, Token = token };
+        }
+
+        // JWT creation method
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                claims: claims, 
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
